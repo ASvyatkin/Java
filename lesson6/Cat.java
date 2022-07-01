a14fdda56e86d51fa7d5f291e270f82f820048f4
ASvyatkin <svyatkin-andrey@mail.ru>
2022-07-01 17:01:02 +0300

    Lesson6
    HomeWork6

---
+++ lesson6/Cat.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.java.lesson6;
+//Created by: Andrey Svyatkin
+//HomeWork6
+public class Cat extends Animal {
+    public Cat(String name) {
+        this.name = name;
+        this.maxRunDistance = 200;
+        this.maxSwimDistance = 0;
+    }
+}
